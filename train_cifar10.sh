07536f9d029c6a34d83638b89484c40e8890ad24
Junho Cho <junhocho@snu.ac.kr>
2021-01-29 17:23:14 +0900

    batchsize of stl10 fixed for 2080 training

--- train_cifar10.sh
+++ train_cifar10.sh
@@ -1,6 +1,6 @@
 # conda activate SCAN
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 python simclr.py --config_env configs/env.yml --config_exp configs/pretext/simclr_cifar10.yml
 # python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_cifar10.yml
 # python selflabel.py --config_env configs/env.yml --config_exp configs/selflabel/selflabel_cifar10.yml
--- train_stl10.sh
+++ train_stl10.sh
@@ -1,6 +1,6 @@
 # conda activate SCAN
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=2
 python simclr.py --config_env configs/env.yml --config_exp configs/pretext/simclr_stl10.yml
 # python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_stl10.yml
 # python selflabel.py --config_env configs/env.yml --config_exp configs/selflabel/selflabel_stl10.yml
