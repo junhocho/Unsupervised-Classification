90c809601276896458a937697fe27ff1c5ddce89
junhocho <junhocho@snu.ac.kr>
2021-01-31 17:53:11 +0900

    yes yes yes for conda-install

--- conda-install.sh
+++ conda-install.sh
@@ -1,12 +1,12 @@
 conda create -n SCAN
 
 
-conda install pytorch=1.4.0 torchvision=0.5.0 cudatoolkit=10.0 -c pytorch
-conda install matplotlib scipy scikit-learn   # For evaluation and confusion matrix visualization
+conda install pytorch=1.4.0 torchvision=0.5.0 cudatoolkit=10.0 -c pytorch -y
+conda install matplotlib scipy scikit-learn -y   # For evaluation and confusion matrix visualization
 pip install faiss-gpu
 
 
 
-conda install pyyaml 
+conda install pyyaml  -y
 pip install  easydict                 # For using config files
-conda install termcolor
+conda install termcolor -y
