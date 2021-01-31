e82cd37b5bd5e5c3ff36d5c86330a554d36fc710
Junho Cho <junhocho@snu.ac.kr>
2021-01-31 17:52:18 +0900

    cifiar, stl train

--- train_cifar10.sh
+++ train_cifar10.sh
@@ -1,9 +1,9 @@
 # conda activate SCAN
 
-export CUDA_VISIBLE_DEVICES=1
-python simclr.py --config_env configs/env.yml --config_exp configs/pretext/simclr_cifar10.yml
+export CUDA_VISIBLE_DEVICES=0
+# python simclr.py --config_env configs/env.yml --config_exp configs/pretext/simclr_cifar10.yml
 # python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_cifar10.yml
-# python selflabel.py --config_env configs/env.yml --config_exp configs/selflabel/selflabel_cifar10.yml
+python selflabel.py --config_env configs/env.yml --config_exp configs/selflabel/selflabel_cifar10.yml
 
 
 # MODEL_PATH_IMAGENET=/home/junho/Image_Clustering/SCAN_Model_Zoo/imagenet.pth.tar
