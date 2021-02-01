19beb4338da55c89e19e25df518406b0e539ca7c
junhocho <junhocho@snu.ac.kr>
2021-02-01 14:48:06 +0900

    imagenet50 fails in BEAT2. unknown reboot issue

---
+++ train_imagenet50.sh
@@ -0,0 +1,14 @@
+# conda activate SCAN
+
+
+# export CUDA_VISIBLE_DEVICES=1,2,3
+python moco.py --config_env configs/env.yml --config_exp configs/pretext/moco_imagenet50.yml
+## DOWNLOADING???
+# python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_imagenet_200.yml
+# python selflabel.py --config_env configs/env.yml --config_exp configs/selflabel/selflabel_cifar10.yml
+
+
+# MODEL_PATH_IMAGENET=/home/junho/Image_Clustering/SCAN_Model_Zoo/imagenet.pth.tar
+# export CUDA_VISIBLE_DEVICES=1
+# # python eval.py --config_exp configs/scan/imagenet_eval.yml --model $MODEL_PATH_IMAGENET
+# python eval.py --config_exp configs/scan/imagenet_eval.yml --model $MODEL_PATH_IMAGENET --visualize_prototypes
