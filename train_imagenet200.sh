b337d93c3240bf114b9a556cd6c12549a0da6d62
junhocho <junhocho@snu.ac.kr>
2021-02-01 16:53:12 +0900

    imagenet200 done. batchsize adjusted

--- train_imagenet200.sh
+++ train_imagenet200.sh
@@ -2,8 +2,8 @@
 
 # python moco.py --config_env configs/env.yml --config_exp configs/pretext/moco_imagenet200.yml
 ## DOWNLOADING???
-python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_imagenet_200.yml
-# python selflabel.py --config_env configs/env.yml --config_exp configs/selflabel/selflabel_cifar10.yml
+# python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_imagenet_200.yml
+python selflabel.py --config_env configs/env.yml --config_exp configs/selflabel/selflabel_imagenet_200.yml
 
 
 # MODEL_PATH_IMAGENET=/home/junho/Image_Clustering/SCAN_Model_Zoo/imagenet.pth.tar
