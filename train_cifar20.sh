491fc0312e3b48728b271b04403f3c7754429d75
Junho Cho <junhocho@snu.ac.kr>
2021-02-02 15:57:52 +0900

    cifar20 (100)

---
+++ train_cifar20.sh
@@ -0,0 +1,6 @@
+# conda activate SCAN
+
+export CUDA_VISIBLE_DEVICES=1
+# python simclr.py --config_env configs/env.yml --config_exp configs/pretext/simclr_cifar20.yml
+# python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_cifar20.yml
+python selflabel.py --config_env configs/env.yml --config_exp configs/selflabel/selflabel_cifar20.yml
