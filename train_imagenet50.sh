7ea31ac91463664ac14cba69bf32525871b417e5
Junho Cho <junhocho@snu.ac.kr>
2021-02-02 09:42:47 +0900

    imagenet50 done in squad3

--- train_imagenet50.sh
+++ train_imagenet50.sh
@@ -2,10 +2,10 @@
 
 
 # export CUDA_VISIBLE_DEVICES=1,2,3
-python moco.py --config_env configs/env.yml --config_exp configs/pretext/moco_imagenet50.yml
+# python moco.py --config_env configs/env.yml --config_exp configs/pretext/moco_imagenet50.yml
 ## DOWNLOADING???
-# python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_imagenet_200.yml
-# python selflabel.py --config_env configs/env.yml --config_exp configs/selflabel/selflabel_cifar10.yml
+# python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_imagenet_50.yml
+python selflabel.py --config_env configs/env.yml --config_exp configs/selflabel/selflabel_imagenet_50.yml
 
 
 # MODEL_PATH_IMAGENET=/home/junho/Image_Clustering/SCAN_Model_Zoo/imagenet.pth.tar
